1534e64b7f0f0168ee6a4e4addcee658f8da740f
Sanne Peters <sanstream@gmail.com>
2015-12-24 14:49:16 +0100

    Initial commit of the code.

---
+++ node-app/main.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+var fs = require('fs');
+var path = require('path');
+var program = require('commander');
+var child_process = require('child_process');
+var ColouredProse  = require('coloured-prose');
+program
+  .version('0.1')
+  .usage('')
+  .parse(process.argv)
+
+var args = program.args;
+
+var sourceDir = '/sources/';
+var targetDir = '/output/';
+
+var vowels = 'a,e,y,u,i,o'.split(',');
+
+
+console.info("Starting the app...");
+
+var files = fs.readdirSync(__dirname + sourceDir);
+var colouredProse = new ColouredProse("a,e,y,i,u,o".split(","));
+
+files.forEach(function(file) {
+  // only read plain/text .txt files:
+  if(/.txt$/.test(file)){
+
+    fs.readFile(__dirname + sourceDir + file, 'utf8', function(err, data) {
+      if(err){
+        console.error(err);
+        return;
+      }
+      colouredProse.processText(data);
+      var sylJson =  JSON.stringify(colouredProse.syllabelisedText);
+      var colouredJson =  JSON.stringify(colouredProse.colourisedText);
+      fs.writeFile(__dirname + targetDir + file + "-syllablised.json", sylJson);
+      fs.writeFile(__dirname + targetDir + file + "-colourised.json", colouredJson);
+    });
+  }
+}, this);
