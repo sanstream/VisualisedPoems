6519754c1b9b03e3b154c81593c19dada4b9ed9c
Sanne Peters <sanstream@gmail.com>
2015-12-30 20:32:04 +0100

    First version of the webapp

--- web-app/source/main.js
+++ web-app/source/main.js
@@ -12,14 +12,31 @@ VisualisedPoems.controller('VisualisedPoem',['$scope', '$attrs', '$http',
 
     $scope.syllabelisedText = null;
     $scope.colourisedText = null;
+    $scope.dataPresent = false;
+
+    $scope.convertToPostion = function (index) {
+      return index * ($scope.dim + 2);
+    };
 
     $scope.init = function () {
       if($attrs.name){
+        var undrScrdName = $attrs.name.replace(/\s/g, '_');
+
         $http({
           method: 'GET',
-          url: '/post-resources/VisualisedPoems/node-app/output/' + $attrs.name.replace(/\s/g, '_') + '-syllabelised.json'
+          url: '/post-resources/VisualisedPoems/node-app/output/' + undrScrdName + '-syllabelised.json'
         }).success(function(data) {
           $scope.syllabelisedText = data;
+        }).error(function(data) {
+          console.error(data);
+        });
+
+        $http({
+          method: 'GET',
+          url: '/post-resources/VisualisedPoems/node-app/output/' + undrScrdName + '-colourised.json'
+        }).success(function(data) {
+          $scope.colourisedText = data;
+          $scope.dataPresent = true;
         }).error(function(data) {
           console.error(data);
         });
